c0b4f4de62b79a9529f7cd2327f4f623372d59a8
Susama012 <susamaswaraj1999@gmail.com>
2025-01-06 23:06:20 +0530

    started backend

---
+++ backend/schema/admin.schema.js
@@ -0,0 +1,8 @@
+import { Schema } from "mongoose"
+
+const adminSchema = new Schema({
+    firstName : { type : String, required: true },
+    lastName : {type : String },
+    email : { type : String, unique : true ,required : true },
+    password : { type: String, required : true }
+})
